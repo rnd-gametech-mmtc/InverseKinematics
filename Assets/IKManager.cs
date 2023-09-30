047e3ec1f1efe0aaa673de1d97e0d38521cb8d4f
Requelme Hugo Rasong <reqh.tp21@mail.mmtc.ac.id>
2023-09-30 10:45:51 +0700

    Done ga bang

---
+++ Assets/IKManager.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class IKManager : MonoBehaviour
+{
+    [Header("Hand IK Targets")]
+    public Transform rightHand;
+    public Transform leftHand;
+    public TwoBoneIKConstraint leftIK;
+    public TwoBoneIKConstraint rightIK;
+
+    Transform target;
+    float direction;
+
+    void Update()
+    {
+        CheckPos(); // Cek posisi tembok kanan kiri
+    }
+
+    void CheckPos()
+    {
+        if (target != null)
+        {
+            Vector3 head = target.position - transform.position;
+            direction = AngleToDirection(transform.forward, head);
+        }
+        else
+            ResetWeight();
+
+    }
+
+    float AngleToDirection(Vector3 forward, Vector3 targetDirection)
+    {
+        Vector3 prep = Vector3.Cross(forward, targetDirection);
+        float dir = Vector3.Dot(prep, transform.up);
+
+        if (dir > 0f)
+        {
+            RaycastPoint(transform.right);
+            rightIK.weight += 2 * Time.deltaTime;
+            leftIK.weight -= 2 * Time.deltaTime;
+            return 1f;
+        }
+        else if (dir < 0f)
+        {
+            RaycastPoint(-transform.right);
+            leftIK.weight += 2 * Time.deltaTime;
+            rightIK.weight -= 2 * Time.deltaTime;
+            return -1f;
+        }
+        else
+        {
+            ResetWeight();
+            return 0f;
+        }
+    } 
+
+    void RaycastPoint(Vector3 direction) // Kalo udah dapet posisi tembok, cari titik permukaannya pake raycast, tempel IK Tangan ke point raycast tadi
+    {
+        Ray ray = new Ray(transform.position, transform.TransformDirection(direction * 3));
+        if (Physics.Raycast(transform.GetComponent<Collider>().bounds.center, direction, out RaycastHit hit, 1f))
+        {
+            leftHand.rotation = Quaternion.identity;
+            leftHand.position = hit.point + new Vector3(0, .5f);
+
+            rightHand.rotation = Quaternion.Euler(180, 360, 0);
+            rightHand.position = hit.point + new Vector3(0, .5f);
+        }
+    }
+
+    void ResetWeight()
+    {
+        leftIK.weight -= 2 * Time.deltaTime;
+        rightIK.weight -= 2 * Time.deltaTime;
+    } // Hapus IK nya kalo udah menjauh dari tembok
+
+    private void OnTriggerEnter(Collider other) // Deteksi collider yang masuk, jadikan variabel tembok
+    {
+        target = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other) // Deteksi collider yang keluar, hapus variabel tembok
+    {
+        target = null;
+    }
+}
