b8b2324ae99d4e77e4af9cfbf0fe3029b92a5e57
Requelme Hugo Rasong <reqh.tp21@mail.mmtc.ac.id>
2023-10-05 22:55:47 +0700

    Animations

---
+++ Assets/Scripts/Procedural Animations/ReadMe.md
@@ -0,0 +1,12 @@
+DISCLAIMER  !!!
+
+
+Script ini belum optimal dan masih dalam pengembangan
+Tidak untuk diujicobakan pada game yang sedang dalam pengerjaan
+
+
+How To Use:
+1. Add Component WallInteractPrototype
+2. Karakter harus sudah memiliki Rig dan memiliki IK
+3. Attach transform dan IK tangan kanan dan kiri ke komponen WallInteractPrototype
+4. Pastikan objek tembok memiliki Rigidbody dan Collider yang menyala.
--- Assets/Scripts/Procedural Animations/WallInteractPrototype.cs
+++ Assets/Scripts/Procedural Animations/WallInteractPrototype.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 
-public class IKManager : MonoBehaviour
+[RequireComponent(typeof(BoxCollider))]
+public class WallInteractPrototype : MonoBehaviour
 {
     [Header("Hand IK Targets")]
     public Transform rightHand;
@@ -11,7 +12,17 @@ public class IKManager : MonoBehaviour
 
     public EnumManager.TagObject tagObject;
     Transform target;
+    BoxCollider boxColl;
     float direction;
+
+    private void Start()
+    {
+        boxColl = GetComponent<BoxCollider>();
+        boxColl.isTrigger = true;
+        Vector3 boxCenter = boxColl.center;
+        boxCenter.y = 1;
+        boxColl.size = new Vector3(1.4f, .5f, .001f);
+    }
 
     void Update()
     {
@@ -28,6 +39,7 @@ public class IKManager : MonoBehaviour
         else
             ResetWeight();
 
+        //RaycastPoint(transform.forward);
     }
 
     float AngleToDirection(Vector3 forward, Vector3 targetDirection)
@@ -58,9 +70,13 @@ public class IKManager : MonoBehaviour
 
     void RaycastPoint(Vector3 direction) // Kalo udah dapet posisi tembok, cari titik permukaannya pake raycast, tempel IK Tangan ke point raycast tadi
     {
-        Ray ray = new Ray(transform.position, transform.TransformDirection(direction * 3));
+        Ray ray = new Ray(transform.position, transform.TransformDirection(direction * 1));
         if (Physics.Raycast(transform.GetComponent<Collider>().bounds.center, direction, out RaycastHit hit, 1f))
         {
+            if(hit.collider.tag == tagObject.ToString())
+            {
+
+            }
             leftHand.rotation = Quaternion.identity;
             leftHand.position = hit.point + new Vector3(0, .5f);
 
